05ca85dd622a73796f4a43611f6716e288fa9628
Steven Woodward <steveanthony999@gmail.com>
2020-05-23 13:58:21 -0700

    added validation

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,37 @@ const express = require('express');
 
 const router = express.Router();
 
+// From express-validator
+const { check, validationResult } = require('express-validator/check');
+
+const User = require('../models/User');
+
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
-router.post('/', (req, res) => {
-  res.send('Registers a user');
-});
+router.post(
+  '/',
+  [
+    check('name', 'Please add name')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({
+      min: 6
+    })
+  ],
+  (req, res) => {
+    //   Only add erros for routes that accept data and need validation
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send('passed');
+  }
+);
 
 module.exports = router;

436a47191304d1dd3188a24e34838c426c754091
Steven Woodward <steveanthony999@gmail.com>
2020-05-23 20:14:54 -0700

    added contact route

--- routes/contacts.js
+++ routes/contacts.js
@@ -31,9 +31,42 @@ router.get('/', auth, async (req, res) => {
 // @route   POST api/contacts
 // @desc    Add new contacts
 // @access  Private
-router.post('/', (req, res) => {
-  res.send('Add contact');
-});
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('name', 'Name is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    //   Only add erros for routes that accept data and need validation
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, phone, type } = req.body;
+
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id
+      });
+
+      const contact = await newContact.save();
+
+      res.json(contact);
+    } catch (err) {
+      console.error(500).send('Server Error');
+    }
+  }
+);
 
 // @route   PUT api/contacts/:id
 // @desc    Update contact
